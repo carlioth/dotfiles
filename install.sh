9e31e25c046d9625b15016a9374e94fab29ff50e
Henrik Carlioth <henrik.carlioth@matchi.se>
2023-03-30 19:56:23 +0200

    fix install script

--- install.sh
+++ install.sh
@@ -12,15 +12,9 @@ brew upgrade
 brew bundle
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-brew install romkatv/powerlevel10k/powerlevel10k
 echo "source $(brew --prefix)/opt/powerlevel10k/powerlevel10k.zsh-theme" >>~/.zshrc
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
-#Add kubectl aliases
-curl -L -o ~/.kubectl_aliases https://rawgit.com/ahmetb/kubectl-alias/master/.kubectl_aliases
-
 brew cleanup
-
